93be3dfa6e115afa6a80860a821ca82046dfb721
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-09-07 22:54:41 +0800

    updit

--- Blog/settings.py
+++ Blog/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # serve static files
+
 ]
 
 ROOT_URLCONF = 'Blog.urls'
