2c8d2e29693f9ba74e0817a4f86974a9b029d8c4
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-09-07 22:36:12 +0800

    test

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('delete/<int:id>/', views.DeletePost, name="delete_post"),
     path('update/<int:id>/', views.UpdatePost, name="update_post"),
     path('register/', views.RegisterUser, name="register"),
-    path('login/', views.LoginView, name="login"),
+    path('/', views.LoginView, name="login"),
     path('logout/', views.Logout, name="logout")
 
 ]
