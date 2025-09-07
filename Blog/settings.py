597db4ef16d82f62053e7c4ba4f4ab878c82d902
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-09-07 22:40:13 +0800

    test

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k^nc4=@i+l+cbpyl7dn#d#znv7e&*2s^l_qp6*gkaq&mlu+ssd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
